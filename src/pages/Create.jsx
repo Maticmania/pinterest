c27bbbe215eab8d4ec38dd8679b7eb32bb2426ce
Matic.dev <olaitantoyeeb1@gmail.com>
2024-05-21 13:05:27 +0100

    layout fixed

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -16,10 +16,9 @@ const Create = () => {
   return (
     <>
       <Header />
-      <div className="h-auto xl:h-[600px] border-t border-gray-500 mt-8 xl:mt-[100px] flex flex-col xl:flex-row gap-2 container mx-auto">
-        <div className="h-full border-gray-500 border-r w-full xl:w-[450px] space-y-4">
+      <div className="h-auto xl:h-[600px] border-t border-gray-500 mt-8 xl:mt-[100px] flex flex-col xl:flex-row gap-2 container mx-auto">        <div className="w-full xl:w-[450px] border-t border-gray-500 xl:border-r flex flex-col space-y-4">
           {/* Create Control */}
-          <div className="h-auto xl:h-[120px] border-b border-gray-400 flex flex-col justify-around">
+          <div className="border-b border-gray-400 flex flex-col justify-around py-4">
             <h1 className="flex justify-between px-4 text-lg font-bold">
               Pin Drafts(1) <span className="">Arrow</span>
             </h1>
@@ -28,7 +27,7 @@ const Create = () => {
             </button>
           </div>
           {/* Draft Pins */}
-          <div className="border-2 border-black h-auto xl:h-[90px] bg-gray-300 flex items-center px-2 gap-4 mx-2 rounded-xl">
+          <div className="border-2 border-black bg-gray-300 flex items-center px-2 gap-4 mx-2 py-4 rounded-xl">
             <img
               src=""
               alt="Pin draft"
@@ -37,8 +36,8 @@ const Create = () => {
             <p>30 days until expiration</p>
           </div>
         </div>
-        <div className="flex h-full w-full flex-col">
-          <div className="border-gray-500 border-b h-auto xl:h-[80px] w-full flex items-center justify-between px-4">
+        <div className="flex flex-col w-full">
+          <div className="border-b border-gray-500 flex items-center justify-between px-4 py-4">
             <p>Create Pin</p>
             <button
               className="text-white bg-red-600 p-3 rounded-3xl px-5 hover:bg-red-700"
@@ -48,7 +47,7 @@ const Create = () => {
             </button>
           </div>
           {/* For Create Pin */}
-          <div className="p-5 overflow-y-auto xl:px-24">
+          <div className="p-5 xl:px-24 overflow-y-auto">
             <CreatePin ref={createPinRef} />
           </div>
         </div>
